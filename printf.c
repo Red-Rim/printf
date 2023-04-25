36f12127881a852c8bbba813b3df65e57e728864
Red-Rim <salmaouakhir20@gmail.com>
2023-04-25 14:16:34 -0700

    la ilaha illa llah

--- printf.c
+++ printf.c
@@ -4,16 +4,12 @@
 /**
  * _printf - a function that produces output according to a format
  * @format: format
- * Return: printed chars
+ * Return: c printed chars
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int c = 0;
-	int num;
-	int num_digits;
-	int divisor;
-	int digit;
 
 
 	va_start(args, format);
@@ -30,30 +26,6 @@ int _printf(const char *format, ...)
 			else if (*format == '%')
 			{
 				c += _putchar('%');
-			}
-			/************print int********************/
-			 else if (*format == 'd' || *format == 'i')
-			 {
-				 num = va_arg(args, int);
-				 if (num < 0)
-				 {
-					 _putchar('-');
-					 c++;
-					 num = -num;
-				 }
-				 num_digits = getNumDigits(num);
-				 c += num_digits;
-				 divisor = computeDivisor(num_digits);
-				 do
-				 {
-					 digit = num / divisor;
-					 _putchar(digit + '0');
-					 c++;
-					 num %= divisor;
-					 divisor /= 10;
-				 }
-				 while (divisor > 0);
-			 }
 			else
 				return (-1);
 		}
